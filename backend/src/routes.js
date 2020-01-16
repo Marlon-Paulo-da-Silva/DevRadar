2d028fe71183f6cdf75ac58284e470e8bea08249
Marlon Paulo <marlon.paulo.silva@outlook.com>
2020-01-16 07:52:07 -0300

    criado rota index do DevController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const routes = Router();
 const DevController = require("./controllers/DevController");
 
+
+routes.get('/devs', DevController.index);
 routes.post("/devs", DevController.store);
 
 module.exports = routes;
