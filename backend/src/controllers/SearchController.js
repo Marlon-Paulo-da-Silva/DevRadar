d789311f284c3a792db1fd6734f52615f631b908
Marlon Paulo <marlon.paulo.silva@outlook.com>
2020-01-16 08:19:29 -0300

    criado filtro SearchController e finalizando primeira versao do backend

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -5,7 +5,25 @@ const parseStringAsArray = require("../utils/parseStringAsArray");
 
 module.exports = {
   async index(req, res) {
-    console.log(req.query);
-    return res.json({ message: "Ok" });
+    const { latitude, longitude, techs } = req.query;
+
+    const techsArray = parseStringAsArray(techs);
+
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [longitude, latitude]
+          },
+          $maxDistance: 10000
+        }
+      }
+    });
+
+    return res.json({ devs });
   }
 };
