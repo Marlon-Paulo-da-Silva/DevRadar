5c1b04fbd42171e963e01acd7fd0c4b325cb851c
Marlon Paulo <marlon.paulo.silva@outlook.com>
2020-01-16 05:35:11 -0300

    criado rotas Devs com busca na

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,18 @@
 const { Router } = require("express");
+const axios = require("axios");
 
 const routes = Router();
 
-routes.get("/", (req, res) => {
+routes.post("/devs", async (req, res) => {
+  const { github_username } = req.body;
+
+  const response = await axios.get(
+    `https://api.github.com/users/${github_username}`
+  );
+
+  const { name = login, avatar_url, bio } = response.data;
+
+  console.log(name, avatar_url, bio);
   return res.json({ message: "Eae Marlon" });
 });
 
