63ec212d56531a1b0735c0cf1c2a33a6007eb12e
Marlon Paulo <marlon.paulo.silva@outlook.com>
2020-01-19 22:57:26 -0300

    adicionando location na mapview

--- mobileExpoReactNative/src/pages/Main.js
+++ mobileExpoReactNative/src/pages/Main.js
@@ -1,13 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 
 import MapView from "react-native-maps";
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync
+} from "expo-location";
 
 function Main() {
+  const [currentRegion, setCurrentRegion] = useState(null);
+
   useEffect(() => {
-    async function loadInitialPosition() {}
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+      console.log(granted);
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04
+        });
+      }
+    }
+
+    loadInitialPosition();
   }, []);
-  return <MapView style={styles.map} />;
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return <MapView initialRegion={currentRegion} style={styles.map} />;
 }
 
 const styles = StyleSheet.create({
