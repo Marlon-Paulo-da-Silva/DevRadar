06ad1309d05ac246d028f1999bf4ee858fe4230c
Marlon Paulo <marlon.paulo.silva@outlook.com>
2020-01-19 23:19:55 -0300

    adicionando avatar no Marker

--- mobileExpoReactNative/src/pages/Main.js
+++ mobileExpoReactNative/src/pages/Main.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Image } from "react-native";
 
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import {
   requestPermissionsAsync,
   getCurrentPositionAsync
@@ -37,12 +37,30 @@ function Main() {
     return null;
   }
 
-  return <MapView initialRegion={currentRegion} style={styles.map} />;
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -22.0700938, longitude: -51.4175201 }}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: "https://avatars2.githubusercontent.com/u/23368031?s=460&v=4"
+          }}
+        />
+      </Marker>
+    </MapView>
+  );
 }
 
 const styles = StyleSheet.create({
   map: {
     flex: 1
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: "#fff"
   }
 });
 
