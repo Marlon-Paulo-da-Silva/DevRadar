2065deecfcedcbca2f69b734610fd53a480b0068
Marlon Paulo <marlon.paulo.silva@outlook.com>
2020-01-20 07:28:47 -0300

    finalizando estilizacao da Main

--- mobileExpoReactNative/src/pages/Main.js
+++ mobileExpoReactNative/src/pages/Main.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Image, View, Text } from "react-native";
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import MapView, { Marker, Callout } from "react-native-maps";
 import {
@@ -38,32 +46,46 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -22.0700938, longitude: -51.4175201 }}>
-        <Image
-          style={styles.avatar}
-          source={{
-            uri: "https://avatars2.githubusercontent.com/u/23368031?s=460&v=4"
-          }}
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -22.0700938, longitude: -51.4175201 }}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: "https://avatars2.githubusercontent.com/u/23368031?s=460&v=4"
+            }}
+          />
+          <Callout
+            onPress={() => {
+              navigation.navigate("Profile", {
+                github_username: "marlon-paulo-da-silva"
+              });
+            }}
+          >
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Marlon Paulo</Text>
+              <Text style={styles.devBio}>
+                Criador de Soluções 🚀 utilizando as melhores tecnologias de
+                desenvolvimento web e mobile.
+              </Text>
+              <Text style={styles.devTechs}>React, React Native, Flutter</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar Devs por techs.."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
         />
-        <Callout
-          onPress={() => {
-            navigation.navigate("Profile", {
-              github_username: "marlon-paulo-da-silva"
-            });
-          }}
-        >
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Marlon Paulo</Text>
-            <Text style={styles.devBio}>
-              Criador de Soluções 🚀 utilizando as melhores tecnologias de
-              desenvolvimento web e mobile.
-            </Text>
-            <Text style={styles.devTechs}>React, React Native, Flutter</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -91,6 +113,39 @@ const styles = StyleSheet.create({
   },
   devTechs: {
     marginTop: 5
+  },
+  searchForm: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: "row"
+  },
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#fff",
+    color: "#333",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 2
+  },
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#8e4dff",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 15
   }
 });
 
